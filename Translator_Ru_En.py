a9a8c8b30a655539f6f8d9129d9a409074af065e
vasilii <you@example.com>
2023-10-16 14:12:30 +0500

    Added str input() to code

--- Translator_Ru_En.py
+++ Translator_Ru_En.py
@@ -6,5 +6,8 @@ from transformers import pipeline
 #Сосзаем пайплан для модели перевода с русского на анлийский язык
 classifier = pipeline(model="Helsinki-NLP/opus-mt-ru-en")
 
-#Выводим результат работы модели
-classifier("Как сделать это задание?")
+#Вводим текст на русском языке для обработки моделью
+text_eng = classifier(input())
+
+#Выводим результат перевода на английском языке
+print(text_eng)
