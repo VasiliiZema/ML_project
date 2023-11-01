e30418ab574856ac1003d4fa67c687a3f0067e37
vasilii <you@example.com>
2023-11-01 16:38:05 +0500

    Create WebTranslator by Streamlit

--- WebTranslator_Streamlit.py
+++ WebTranslator_Streamlit.py
@@ -4,7 +4,7 @@ import streamlit as st
 classifier = pipeline(model="Helsinki-NLP/opus-mt-ru-en")
 st.title('Перевод текста на английский язык')
 text = st.text_input('Введите текст на русском языке')
-result = st.button('Нажми, что бы перевести')
+result = st.button('Нажми, что бы перевести!')
 if result:
     text_eng = classifier(text)
     st.subheader('Перевод:')
